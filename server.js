c09d1840b2b2c794b575eedd8caf6133d5e757de
rojefferson <rojefferson@outlook.com>
2025-03-29 18:25:04 -0300

    AJUSTES

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // CORS totalmente liberado
 app.use(cors({
   origin: '*'
 }));
 
-app.get('/unidades', (req, res) => {
+app.get('/', (req, res) => {
     const results = [];
 
     fs.createReadStream('unidades-basica-saude.csv')
